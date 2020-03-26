2cac57879779e85794345e3b583c8964818d4c6d
jagdish.banda <jagdish.banda@openxcelltechnolabs.com>
2020-03-26 19:02:10 +0530

    Add usd flag in conversion

--- src/controllers/v1/FixApiController.js
+++ src/controllers/v1/FixApiController.js
@@ -68,6 +68,11 @@ class FixApiController extends AppController {
       req_body.user_id = user_id;
       var jstResponseValue = await FixApiHelper.priceObject(req_body);
       jstResponseValue.faldax_fee = jstResponseValue.faldax_fee;
+      if( usd_value == "" || usd_value == null ){
+        jstResponseValue.is_usd_set = false;
+      }else{
+        jstResponseValue.is_usd_set = true;
+      }
       return jstResponseValue;
     } catch (err) {
       console.log("err", err);
