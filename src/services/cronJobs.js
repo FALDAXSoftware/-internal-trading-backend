7878bb6d11a663f806d1dafb9e09e94d606708ad
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2020-05-01 14:18:00 +0530

    try catch and cancel pending data

--- src/services/cronJobs.js
+++ src/services/cronJobs.js
@@ -20,12 +20,12 @@ cron.schedule('*/2 * * * *', async (req, res, next) => {
 //     await dashBoardUpdate.updateSellOrderBook("LTC-BTC");
 // });
 
-cron.schedule('*/15 * * * * *', async (req, res, next) => {
+cron.schedule('* * * * *', async (req, res, next) => {
     console.log("INISDER FGJ");
     await dashBoardUpdate.deletePendingOrder();
 })
 
-cron.schedule('*/15 * * * * *', async (req, res, next) => {
+cron.schedule('* * * * *', async (req, res, next) => {
     console.log("INISDER FGJ");
     await dashBoardUpdate.deleteSellPendingOrder();
 })
