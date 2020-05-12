68eb00a82f7a1f718e8168e55e072446580f4969
kalpit <kalpit.akhawat@openxcell.info>
2020-05-12 12:10:17 +0530

    trade screen optimization

--- src/helpers/chart/get-depth-chart-detail.js
+++ src/helpers/chart/get-depth-chart-detail.js
@@ -3,22 +3,26 @@ Get depth chart detail
 */
 var BuyBookModel = require("../../models/BuyBook");
 var SellBookModel = require("../../models/SellBook");
-
+const { raw } = require('objection');
 var getDepthChartDetails = async (crypto, currency, limit = 100) => {
     var depthChartDetail;
     var buyDetails = await BuyBookModel
         .query()
+        .select("price", raw("SUM(quantity) as quantity"))
         .where('settle_currency', crypto)
         .andWhere('currency', currency)
         .andWhere('deleted_at', null)
+        .groupBy("price")
         .orderBy('price', 'DESC')
         .limit(limit);
 
     var sellDetails = await SellBookModel
         .query()
+        .select("price", raw("SUM(quantity) as quantity"))
         .where('settle_currency', crypto)
         .andWhere('currency', currency)
         .andWhere('deleted_at', null)
+        .groupBy("price")
         .orderBy('price', 'ASC')
         .limit(limit);
 
