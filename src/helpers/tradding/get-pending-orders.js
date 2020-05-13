c4c09c2506eb4ca8d87bf15dade6ca82c0ecb40e
jagdish.banda <jagdish.banda@openxcelltechnolabs.com>
2020-05-13 18:25:09 +0530

    Completed/Cancelled orders list

--- src/helpers/tradding/get-pending-orders.js
+++ src/helpers/tradding/get-pending-orders.js
@@ -140,8 +140,29 @@ var getPendingOrders = async (user_id, crypto, currency, month, limit = 100) => 
     }
     return tradePendingDetails;
 
+},
+var getUserPendingOrders = async (user_id, crypto, currency, limit = 100, page, fromDate, toDate) => {
+    var tradePendingDetails;
+    var yesterday = moment
+        .utc()
+        .subtract(month, 'months')
+        .format();
+    var pendingSql = `SELECT pending_orders.* FROM (
+                                SELECT id, CAST(user_id AS int) as user_id,order_type, fill_price, limit_price, stop_price, quantity, currency, settle_currency, side, created_at, placed_by FROM pending_book WHERE deleted_at IS NULL AND settle_currency='${crypto}' AND currency='${currency}' AND user_id='${user_id}' AND created_at >= '${yesterday}'
+                                UNION ALL
+                                SELECT id, user_id,order_type, fill_price, limit_price, stop_price, quantity, currency, settle_currency, side, created_at, placed_by FROM buy_book WHERE deleted_at IS NULL AND settle_currency='${crypto}' AND currency='${currency}' AND user_id=${user_id} AND is_partially_fulfilled='true' AND created_at >= '${yesterday}'
+                                UNION ALL
+                                SELECT id, user_id,order_type, fill_price, limit_price, stop_price, quantity, currency, settle_currency, side, created_at, placed_by FROM sell_book WHERE deleted_at IS NULL AND settle_currency='${crypto}' AND currency='${currency}' AND user_id=${user_id} AND is_partially_fulfilled='true' AND created_at >= '${yesterday}'
+                        ) as pending_orders WHERE created_at >= '${yesterday}' ORDER BY created_at DESC LIMIT ${page}, ${limit}`
+
+    tradePendingDetails = await PendingBookModel.knex().raw(pendingSql);
+    tradePendingDetails = tradePendingDetails.rows;
+
+    return tradePendingDetails;
+
 }
 
 module.exports = {
-    getPendingOrders
+    getPendingOrders,
+    getUserPendingOrders
 }
