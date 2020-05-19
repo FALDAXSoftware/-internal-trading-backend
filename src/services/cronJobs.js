4b57cc2d19cbf029e74fa02ee7503a6df19aaac6
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2020-05-19 14:24:16 +0530

    dahsorad pair changes

--- src/services/cronJobs.js
+++ src/services/cronJobs.js
@@ -10,7 +10,7 @@ cron.schedule('*/2 * * * *', async (req, res, next) => {
     await cronData.executeStopLimit();
 });
 
-cron.schedule('*/30 * * * * *', async (req, res, next) => {
+cron.schedule('*/15 * * * * *', async (req, res, next) => {
     console.log("Started cron....");
     await dashBoardUpdate.updateBuyOrderBook("LTC-BTC");
 });
