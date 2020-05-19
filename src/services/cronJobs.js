8d5ac08392fd28e1a8b6622e766f26e489d80c02
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2020-05-19 20:23:30 +0530

    cron for another pair

--- src/services/cronJobs.js
+++ src/services/cronJobs.js
@@ -32,25 +32,25 @@ cron.schedule('* * * * *', async (req, res, next) => {
 })
 
 // Cron For XRP-BTC
-// cron.schedule('*/15 * * * * *', async (req, res, next) => {
-//     console.log("Started cron....");
-//     await dashBoardUpdate.updateBuyOrderBook("XRP-BTC");
-// });
+cron.schedule('*/15 * * * * *', async (req, res, next) => {
+    console.log("Started cron....");
+    await dashBoardUpdate.updateBuyOrderBook("XRP-BTC");
+});
 
-// cron.schedule('*/15 * * * * *', async (req, res, next) => {
-//     console.log("Started cron....");
-//     await dashBoardUpdate.updateSellOrderBook("XRP-BTC");
-// });
+cron.schedule('*/15 * * * * *', async (req, res, next) => {
+    console.log("Started cron....");
+    await dashBoardUpdate.updateSellOrderBook("XRP-BTC");
+});
 
-// cron.schedule('* * * * *', async (req, res, next) => {
-//     console.log("INISDER FGJ");
-//     await dashBoardUpdate.deletePendingOrder("XRP-BTC");
-// })
+cron.schedule('* * * * *', async (req, res, next) => {
+    console.log("INISDER FGJ");
+    await dashBoardUpdate.deletePendingOrder("XRP-BTC");
+})
 
-// cron.schedule('* * * * *', async (req, res, next) => {
-//     console.log("INISDER FGJ");
-//     await dashBoardUpdate.deleteSellPendingOrder("XRP-BTC");
-// })
+cron.schedule('* * * * *', async (req, res, next) => {
+    console.log("INISDER FGJ");
+    await dashBoardUpdate.deleteSellPendingOrder("XRP-BTC");
+})
 
 // //  Cron for ETH-BTC
 // cron.schedule('*/15 * * * * *', async (req, res, next) => {
