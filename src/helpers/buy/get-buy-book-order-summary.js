90bacb176740ea7015aa27fe1bade5342553fd22
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2020-05-22 18:25:34 +0530

    queue changes

--- src/helpers/buy/get-buy-book-order-summary.js
+++ src/helpers/buy/get-buy-book-order-summary.js
@@ -8,8 +8,8 @@ var getBuyBookOrderSummary = async (crypto, currency) => {
         .query()
         .select('price', raw('SUM(quantity) as quantity'))
         .where('deleted_at', null)
-        .andWhere('quantity', '>', 0 )
-        .andWhere('limit_price', '>', 0 )
+        .andWhere('quantity', '>', 0)
+        .andWhere('limit_price', '>', 0)
         .andWhere('settle_currency', crypto)
         .andWhere('currency', currency)
         .groupBy('price')
@@ -18,7 +18,8 @@ var getBuyBookOrderSummary = async (crypto, currency) => {
 
     var totalSql = `SELECT SUM(price * quantity) as total
                         FROM buy_book WHERE settle_currency='${crypto}' AND currency='${currency}'
-                        AND deleted_at IS NULL`
+                        AND deleted_at IS NULL AND quantity > 0 AND limit_price > 0 
+                        LIMIT 100`
 
     var totalData = await BuyBookModel.knex().raw(totalSql)
 
--- src/helpers/sell/get-sell-book-order-summary.js
+++ src/helpers/sell/get-sell-book-order-summary.js
@@ -7,8 +7,8 @@ var sellOrderBookSummary = async (crypto, currency) => {
         .query()
         .select('price', raw('SUM(quantity) as quantity'))
         .where('deleted_at', null)
-        .andWhere('quantity', '>', 0 )
-        .andWhere('limit_price', '>', 0 )
+        .andWhere('quantity', '>', 0)
+        .andWhere('limit_price', '>', 0)
         .andWhere('settle_currency', crypto)
         .andWhere('currency', currency)
         .groupBy('price')
@@ -16,7 +16,8 @@ var sellOrderBookSummary = async (crypto, currency) => {
 
     var totalSql = `SELECT SUM(quantity) as total
                         FROM sell_book WHERE settle_currency='${crypto}' AND currency='${currency}'
-                        AND deleted_at IS NULL`
+                        AND deleted_at IS NULL AND quantity > 0 AND limit_price > 0 
+                        LIMIT 100`
 
     var totalData = await SellBookModel.knex().raw(totalSql)
 
