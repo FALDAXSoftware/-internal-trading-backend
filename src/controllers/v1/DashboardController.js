9ab7fe2804468411ae4d3809da411522e366d278
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2020-05-25 13:48:48 +0530

    stop limit and dashboard amount changes

--- src/controllers/v1/DashboardController.js
+++ src/controllers/v1/DashboardController.js
@@ -265,21 +265,25 @@ class DashboardController extends AppController {
 
                 if (maxValue.bot_status) {
 
-                    // var getCryptoValue = await CurrencyConversionModel
-                    //     .query()
-                    //     .first()
-                    //     .select()
-                    //     .where("deleted_at", null)
-                    //     .andWhere("symbol", "LIKE", '%' + crypto + '%')
-                    //     .orderBy("id", "DESC");
+                    var getCryptoValue = await CurrencyConversionModel
+                        .query()
+                        .first()
+                        .select()
+                        .where("deleted_at", null)
+                        .andWhere("symbol", "LIKE", '%' + crypto + '%')
+                        .orderBy("id", "DESC");
 
-                    // var usdValue = getCryptoValue.quote.USD.price
-                    // var min = (maxValue.crypto_minimum) / (usdValue);
-                    // var max = (maxValue.crypto_maximum) / (usdValue);
-                    // for (var i = 0; i < bidValue.length; i++) {
-                    //     var highlightedNumber = Math.random() * (max - min) + min;
-                    //     bidValue[i][1] = highlightedNumber
-                    // }
+                    var usdValue = getCryptoValue.quote.USD.price
+                    var min = (maxValue.crypto_minimum) / (usdValue);
+                    var max = (maxValue.crypto_maximum) / (usdValue);
+                    for (var i = 0; i < bidValue.length; i++) {
+                        if (bidValue[i][1] > max) {
+                            var highlightedNumber = Math.random() * (max - min) + min;
+                            bidValue[i][1] = highlightedNumber
+                        } else {
+                            bidValue[i][1] = bidValue[i][1]
+                        }
+                    }
                     var now = new Date();
                     let requestedWallets = await CoinsModel
                         .query()
@@ -396,22 +400,26 @@ class DashboardController extends AppController {
                     .orderBy("id", 'DESC')
                 if (maxValue.bot_status) {
 
-                    // var getCryptoValue = await CurrencyConversionModel
-                    //     .query()
-                    //     .first()
-                    //     .select()
-                    //     .where("deleted_at", null)
-                    //     .andWhere("symbol", "LIKE", '%' + crypto + '%')
-                    //     .orderBy("id", "DESC");
+                    var getCryptoValue = await CurrencyConversionModel
+                        .query()
+                        .first()
+                        .select()
+                        .where("deleted_at", null)
+                        .andWhere("symbol", "LIKE", '%' + crypto + '%')
+                        .orderBy("id", "DESC");
 
-                    // var usdValue = getCryptoValue.quote.USD.price
-                    // var min = (maxValue.crypto_minimum) / (usdValue);
-                    // var max = (maxValue.crypto_maximum) / (usdValue);
+                    var usdValue = getCryptoValue.quote.USD.price
+                    var min = (maxValue.crypto_minimum) / (usdValue);
+                    var max = (maxValue.crypto_maximum) / (usdValue);
 
-                    // for (var i = 0; i < askValue.length; i++) {
-                    //     var highlightedNumber = Math.random() * (max - min) + min;
-                    //     askValue[i][1] = highlightedNumber
-                    // }
+                    for (var i = 0; i < askValue.length; i++) {
+                        if (askValue[i][1] > max) {
+                            var highlightedNumber = Math.random() * (max - min) + min;
+                            askValue[i][1] = highlightedNumber
+                        } else {
+                            askValue[i][1] = askValue[i][1]
+                        }
+                    }
 
                     var now = new Date();
                     let requestedWallets = await CoinsModel
