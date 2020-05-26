add120b3ad270ad8ffac7a6af04f2d6bdc1ed8ae
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2020-05-26 10:30:21 +0530

    cron and buy order changes

--- src/helpers/buy/get-buy-book-order-summary.js
+++ src/helpers/buy/get-buy-book-order-summary.js
@@ -18,14 +18,21 @@ var getBuyBookOrderSummary = async (crypto, currency) => {
 
     var totalSql = `SELECT SUM(price * quantity) as total
                         FROM buy_book WHERE settle_currency='${crypto}' AND currency='${currency}'
-                        AND deleted_at IS NULL AND quantity > 0 AND limit_price > 0 
+                        AND deleted_at IS NULL AND quantity > 0 AND limit_price > 0
                         LIMIT 100`
 
-    var totalData = await BuyBookModel.knex().raw(totalSql)
+    var totalData = await BuyBookModel.knex().raw(totalSql);
+
+    var totalQuantitySql = `SELECT SUM(quantity) as total_value
+                                FROM buy_book WHERE settle_currency='${crypto}' AND currency='${currency}'
+                                AND deleted_at IS NULL AND quantity > 0 AND limit_price > 0
+                                LIMIT 100`
+    var totalQuanityData = await BuyBookModel.knex().raw(totalQuantitySql);
 
     var buyTotal = {
         "data": buyBookOrders,
-        "total": totalData.rows[0].total
+        "total": totalData.rows[0].total,
+        "total_quantity": totalQuanityData.rows[0].total_value
     }
 
     return (buyTotal)
