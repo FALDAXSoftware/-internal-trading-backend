1b2cff058a81321c928279b59a40b90c1e4e0f62
Mansi Gyastelwala <mansi.gyastelwala@openxcellinc.com>
2020-05-28 12:02:46 +0000

    Update QueueController.js

--- src/controllers/v1/QueueController.js
+++ src/controllers/v1/QueueController.js
@@ -10,68 +10,68 @@ amqp.connect(CONN_URL, opt, (err, conn) => {
         channel.prefetch(3)
         ch = channel;
         console.log("process.env.QUEUE_NAME", process.env.QUEUE_NAME)
-        // ch.consume(process.env.QUEUE_NAME, (msg, err) => {
-        //     // console.log("mesages");
-        //     console.log(err)
-        //     console.log("Message", msg.content.toString())
-        //     var tradeData = require("./TradeController");
-        //     var dataValue = JSON.parse(msg.content.toString());
-        //     var type = dataValue.order_type
-        //     console.log("dataValue", dataValue)
-        //     switch (type) {
-        //         case "Market":
-        //             if (dataValue.side == "Buy") {
-        //                 tradeData.makeMarketBuyOrder(dataValue.symbol, dataValue.side, dataValue.order_type, dataValue.orderQuantity, dataValue.user_id, dataValue.res, dataValue.crypto, dataValue.currency)
-        //                     .then((orderDataResponse) => {
-        //                         console.log("orderDataResponse", orderDataResponse)
-        //                         ch.ack(msg)
-        //                     })
-        //                     .catch((err) => {
-        //                         console.log(err)
-        //                         ch.ack(msg)
-        //                     })
-        //                 break;
-        //             } else if (dataValue.side == "Sell") {
-        //                 tradeData.makeMarketSellOrder(dataValue.res, dataValue.object, dataValue.crypto, dataValue.currency)
-        //                     .then((orderDataResponse) => {
-        //                         console.log("orderDataResponse", orderDataResponse)
-        //                         ch.ack(msg)
-        //                     })
-        //                     .catch((err) => {
-        //                         console.log(err)
-        //                         ch.ack(msg)
-        //                     })
-        //                 break;
-        //             }
-        //         case "Limit":
-        //             if (dataValue.side == "Buy") {
-        //                 tradeData.limitBuyOrder(dataValue.symbol, dataValue.user_id, dataValue.side, dataValue.order_type, dataValue.orderQuantity, dataValue.limit_price, dataValue.res, dataValue.flag, dataValue.crypto, dataValue.currency)
-        //                     .then((orderDataResponse) => {
-        //                         console.log("orderDataResponse", orderDataResponse)
-        //                         ch.ack(msg)
-        //                     })
-        //                     .catch((err) => {
-        //                         console.log(err)
-        //                         ch.ack(msg)
-        //                     })
-        //                 break;
-        //             } else if (dataValue.side == "Sell") {
-        //                 tradeData.limitSellOrder(dataValue.symbol, dataValue.user_id, dataValue.side, dataValue.order_type, dataValue.orderQuantity, dataValue.limit_price, dataValue.res, dataValue.flag, dataValue.crypto, dataValue.currency)
-        //                     .then((orderDataResponse) => {
-        //                         console.log("orderDataResponse", orderDataResponse)
-        //                         ch.ack(msg)
-        //                     })
-        //                     .catch((err) => {
-        //                         console.log(err)
-        //                         ch.ack(msg)
-        //                     })
-        //                 break;
-        //             }
-        //         // break;
-        //         default:
-        //             break;
-        //     }
-        // }, { noAck: false })
+        ch.consume(process.env.QUEUE_NAME, (msg, err) => {
+            // console.log("mesages");
+            console.log(err)
+            console.log("Message", msg.content.toString())
+            var tradeData = require("./TradeController");
+            var dataValue = JSON.parse(msg.content.toString());
+            var type = dataValue.order_type
+            console.log("dataValue", dataValue)
+            switch (type) {
+                case "Market":
+                    if (dataValue.side == "Buy") {
+                        tradeData.makeMarketBuyOrder(dataValue.symbol, dataValue.side, dataValue.order_type, dataValue.orderQuantity, dataValue.user_id, dataValue.res, dataValue.crypto, dataValue.currency)
+                            .then((orderDataResponse) => {
+                                console.log("orderDataResponse", orderDataResponse)
+                                ch.ack(msg)
+                            })
+                            .catch((err) => {
+                                console.log(err)
+                                ch.ack(msg)
+                            })
+                        break;
+                    } else if (dataValue.side == "Sell") {
+                        tradeData.makeMarketSellOrder(dataValue.res, dataValue.object, dataValue.crypto, dataValue.currency)
+                            .then((orderDataResponse) => {
+                                console.log("orderDataResponse", orderDataResponse)
+                                ch.ack(msg)
+                            })
+                            .catch((err) => {
+                                console.log(err)
+                                ch.ack(msg)
+                            })
+                        break;
+                    }
+                case "Limit":
+                    if (dataValue.side == "Buy") {
+                        tradeData.limitBuyOrder(dataValue.symbol, dataValue.user_id, dataValue.side, dataValue.order_type, dataValue.orderQuantity, dataValue.limit_price, dataValue.res, dataValue.flag, dataValue.crypto, dataValue.currency)
+                            .then((orderDataResponse) => {
+                                console.log("orderDataResponse", orderDataResponse)
+                                ch.ack(msg)
+                            })
+                            .catch((err) => {
+                                console.log(err)
+                                ch.ack(msg)
+                            })
+                        break;
+                    } else if (dataValue.side == "Sell") {
+                        tradeData.limitSellOrder(dataValue.symbol, dataValue.user_id, dataValue.side, dataValue.order_type, dataValue.orderQuantity, dataValue.limit_price, dataValue.res, dataValue.flag, dataValue.crypto, dataValue.currency)
+                            .then((orderDataResponse) => {
+                                console.log("orderDataResponse", orderDataResponse)
+                                ch.ack(msg)
+                            })
+                            .catch((err) => {
+                                console.log(err)
+                                ch.ack(msg)
+                            })
+                        break;
+                    }
+                // break;
+                default:
+                    break;
+            }
+        }, { noAck: false })
         // conn.close();
     });
 });
