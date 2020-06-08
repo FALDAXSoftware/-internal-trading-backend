421e11067c6f6a4179f34471cd781e6ec486404b
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2020-06-08 16:42:42 +0530

    get sell book and buy book data

--- src/helpers/buy/get-buy-book-order-summary.js
+++ src/helpers/buy/get-buy-book-order-summary.js
@@ -34,19 +34,27 @@ var getBuyBookOrderSummary = async (crypto, currency) => {
 
     // console.log("BUY BOK FUNCTION", buyBookOrdersObjection)
 
-    var totalSql = await BuyBookModel
-        .query()
-        // .select(raw('SUM(quantity) as total_value_1'), raw('SUM(quantity * price) as total_1'))
-        .sum('quantity as total_value')
-        .sum({ total: raw('(quantity * price)') })
-        .where('deleted_at', null)
-        .andWhere('quantity', '>', 0)
-        .andWhere('limit_price', '>', 0)
-        .andWhere('settle_currency', crypto)
-        .andWhere('currency', currency)
-        .limit(100);
+    // var totalSql = await BuyBookModel
+    //     .query()
+    //     // .select(raw('SUM(quantity) as total_value_1'), raw('SUM(quantity * price) as total_1'))
+    //     .sum('quantity as total_value')
+    //     .sum({ total: raw('(quantity * price)') })
+    //     .where('deleted_at', null)
+    //     .andWhere('quantity', '>', 0)
+    //     .andWhere('limit_price', '>', 0)
+    //     .andWhere('settle_currency', crypto)
+    //     .andWhere('currency', currency)
+    //     .limit(100);
 
-    console.log("totalSql", totalSql)
+    // console.log("totalSql", totalSql)
+
+    var totalQuantity = 0.0;
+    var totalQuantityFill = 0.0;
+    for (let index = 0; index < buyBookOrders.length; index++) {
+        const element = buyBookOrders[index];
+        totalQuantity = totalQuantity + element.quantity;
+        totalQuantityFill = totalQuantityFill + (element.quantity * element.fill_price)
+    }
 
     // var totalSql = `SELECT SUM(price * quantity) as total
     //                     FROM buy_book WHERE settle_currency='${crypto}' AND currency='${currency}'
@@ -65,8 +73,8 @@ var getBuyBookOrderSummary = async (crypto, currency) => {
 
     var buyTotal = {
         "data": buyBookOrders,
-        "total": totalSql[0].total,
-        "total_quantity": totalSql[0].total_value
+        "total": totalQuantityFill,
+        "total_quantity": totalQuantity
     }
 
     return (buyTotal)
