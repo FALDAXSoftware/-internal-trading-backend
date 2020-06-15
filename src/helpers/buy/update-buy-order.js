9458fe8a1155a16f5cc9b1071f84a0f5c6525917
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2020-06-15 18:27:51 +0530

    update buy order

--- src/helpers/buy/update-buy-order.js
+++ src/helpers/buy/update-buy-order.js
@@ -2,19 +2,19 @@ var BuyBookModel = require("../../models/BuyBook");
 
 var updateBuyBook = async (id, orderData) => {
 
-    var sqlQuery = await BuyBookModel.knex().raw(`UPDATE buy_book SET quantity = ${orderData.quantity}
-                                                    WHERE CASE WHEN id = ${id} THEN pg_try_advisory_xact_lock(id) END
-                                                    RETURNING *`)
-    sqlQuery = sqlQuery.rows[0];
-    console.log("sqlQuery", sqlQuery)
+    // var sqlQuery = await BuyBookModel.knex().raw(`UPDATE buy_book SET quantity = ${orderData.quantity}
+    //                                                 WHERE CASE WHEN id = ${id} THEN pg_try_advisory_xact_lock(id) END
+    //                                                 RETURNING *`)
+    // sqlQuery = sqlQuery.rows[0];
+    // console.log("sqlQuery", sqlQuery)
 
-    // var updateBook = await BuyBookModel
-    //     .query()
-    //     .patchAndFetchById(id,
-    //         orderData
-    //     );
+    var updateBook = await BuyBookModel
+        .query()
+        .patchAndFetchById(id,
+            orderData
+        );
 
-    return (sqlQuery)
+    return (updateBook)
 }
 
 module.exports = {
